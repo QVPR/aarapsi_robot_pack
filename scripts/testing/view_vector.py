f0466b34458c2d5af34108ef8647fba92b25c286
Owen Claxton <claxtono@qut.edu.au>
2023-04-06 16:34:22 +1000

    added derivative inspection mode

--- scripts/testing/view_vector.py
+++ scripts/testing/view_vector.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import rospy
-from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
 from sensor_msgs.msg import Imu
 from tf.transformations import euler_from_quaternion
@@ -11,6 +10,7 @@ matplotlib.use('Qt5agg')
 import matplotlib.pyplot as plt
 import numpy as np
 import argparse as ap
+import warnings
 
 from pyaarapsi.core.argparse_tools import check_positive_float, check_string, check_bool
 
@@ -18,7 +18,9 @@ class Viewer:
     def __init__(self, node_name, anon, log_level, rate, mode, topic=None):
         rospy.init_node(node_name, anonymous=anon, log_level=log_level)
         rospy.loginfo('Starting %s node.' % (node_name))
-
+        rospy.logdebug("PARAMETERS:\n\t\t\t\tNode Name: %s\n\t\t\t\tMode: %s\n\t\t\t\tAnonymous: %s\n\t\t\t\tLogging Level: %s\n\t\t\t\tRate: %s\n\t\t\t\tTopic: %s" \
+                   % (str(node_name), str(mode), str(anon), str(log_level), str(rate), str(topic)))
+            
         self.node_name  = node_name
         self.anon       = anon
         self.log_level  = log_level
@@ -31,11 +33,16 @@ class Viewer:
         self.msg        = msg
         self.new_msg    = True
 
+    def yaw_from_q(self, orientation):
+        return euler_from_quaternion([float(orientation.x), float(orientation.y), float(orientation.z), float(orientation.w)])[2]
+
     def main(self):
         if self.mode == "odom":
             self.main_odometry()
         elif self.mode == "imu": 
             self.main_imu()
+        elif self.mode == 'dOdom':
+            self.main_odometry_derivative()
 
     def main_imu(self):
 
@@ -99,6 +106,117 @@ class Viewer:
             # update plot limits:
             self.axes_twins[0][0].set_ylim(mins_dict['yaw'],    maxs_dict['yaw'])
             self.axes_twins[0][1].set_ylim(mins_dict['vyaw'],   maxs_dict['vyaw'])
+
+            # draw:
+            self.fig.canvas.draw_idle()
+            self.fig.canvas.start_event_loop(0.001)
+
+    def main_odometry_derivative(self):
+        
+        if self.topic is None:
+            self.topic_to_view = "/odometry/filtered"
+        else:
+            self.topic_to_view = self.topic
+        
+        self.sub = rospy.Subscriber(self.topic_to_view, Odometry, self.topic_cb, queue_size=1)
+
+        self.fig, self.axes = plt.subplots(3,1)
+        self.fig.show()
+        self.axes_twins = [[i, i.twinx()] for i in self.axes]
+
+        self.msg        = None
+        self.omsg    = None
+        self.new_msg    = False
+        self.old_msg    = False
+
+        data_dict   = {'x': [], 'y': [], 'yaw': [], 'vx': [], 'vy': [], 'vyaw': []}
+        _MIN = 10000
+        _MAX = -10000
+        mins_dict = {'x': _MIN, 'y': _MIN, 'yaw': _MIN, 'vx': _MIN, 'vy': _MIN, 'vyaw': _MIN}
+        maxs_dict = {'x': _MAX, 'y': _MAX, 'yaw': _MAX, 'vx': _MAX, 'vy': _MAX, 'vyaw': _MAX}
+        hist_len    = 100
+
+        while not rospy.is_shutdown():
+            self.rate_obj.sleep() # reduce cpu load
+            if not (self.new_msg and self.old_msg):
+                if self.new_msg:
+                    self.omsg = self.msg
+                    self.old_msg = True
+                continue #denest
+            self.new_msg = False
+
+            ## todo each loop:
+            # get new data:
+            new____x = round(self.msg.pose.pose.position.x,3)
+            new____y = round(self.msg.pose.pose.position.y,3)
+            new__yaw = round(self.yaw_from_q(self.msg.pose.pose.orientation), 3)
+            new___vx = round(self.msg.pose.pose.position.x-self.omsg.pose.pose.position.x,3)
+            new___vy = round(self.msg.pose.pose.position.y-self.omsg.pose.pose.position.y,3)
+            new_vyaw = round(self.yaw_from_q(self.msg.pose.pose.orientation) - self.yaw_from_q(self.omsg.pose.pose.orientation),3)
+            self.omsg = self.msg
+
+            # store new data:
+            data_dict['x'].append(new____x)
+            data_dict['y'].append(new____y)
+            data_dict['yaw'].append(new__yaw)
+            data_dict['vx'].append(new___vx)
+            data_dict['vy'].append(new___vy)
+            data_dict['vyaw'].append(new_vyaw)
+
+            # crunch plot limits:
+            mins_dict = {key: data_dict[key][-1] if data_dict[key][-1] < mins_dict[key] else mins_dict[key] for key in list(mins_dict.keys())}
+            maxs_dict = {key: data_dict[key][-1] if data_dict[key][-1] > maxs_dict[key] else maxs_dict[key] for key in list(maxs_dict.keys())}
+
+            # clean axes:
+            length_arr = len(data_dict['x'])
+            while length_arr > hist_len:
+                [data_dict[key].pop(0) for key in list(data_dict.keys())]
+                length_arr -= 1
+            spacing = np.arange(length_arr)
+            [[j.clear() for j in i] for i in self.axes_twins] # clear old data from axes
+
+            if length_arr < 10:
+                continue
+
+            # plot data:
+            self.axes_twins[0][0].plot(spacing, data_dict['x'], 'r')
+            self.axes_twins[0][1].plot(spacing, data_dict['vx'], 'b')
+            self.axes_twins[1][0].plot(spacing, data_dict['y'], 'r')
+            self.axes_twins[1][1].plot(spacing, data_dict['vy'], 'b')
+            self.axes_twins[2][0].plot(spacing, data_dict['yaw'], 'r')
+            self.axes_twins[2][1].plot(spacing, data_dict['vyaw'], 'b')
+
+            # update plot limits:
+            E = 0.2
+            with warnings.catch_warnings():
+                warnings.simplefilter("ignore")
+                self.axes_twins[0][0].set_ylim(mins_dict['x']    - (maxs_dict['x']    - mins_dict['x'])*E,    maxs_dict['x']    + (maxs_dict['x']    - mins_dict['x'])*E)
+                self.axes_twins[0][1].set_ylim(mins_dict['vx']   - (maxs_dict['vx']   - mins_dict['vx'])*E,   maxs_dict['vx']   + (maxs_dict['vx']   - mins_dict['vx'])*E)
+                self.axes_twins[1][0].set_ylim(mins_dict['y']    - (maxs_dict['y']    - mins_dict['y'])*E,    maxs_dict['y']    + (maxs_dict['y']    - mins_dict['y'])*E)
+                self.axes_twins[1][1].set_ylim(mins_dict['vy']   - (maxs_dict['vy']   - mins_dict['vy'])*E,   maxs_dict['vy']   + (maxs_dict['vy']   - mins_dict['vy'])*E)
+                self.axes_twins[2][0].set_ylim(mins_dict['yaw']  - (maxs_dict['yaw']  - mins_dict['yaw'])*E,  maxs_dict['yaw']  + (maxs_dict['yaw']  - mins_dict['yaw'])*E)
+                self.axes_twins[2][1].set_ylim(mins_dict['vyaw'] - (maxs_dict['vyaw'] - mins_dict['vyaw'])*E, maxs_dict['vyaw'] + (maxs_dict['vyaw'] - mins_dict['vyaw'])*E)
+            
+            # disable ticks and labels on x axis
+            self.axes_twins[0][0].set_xticks([])
+            self.axes_twins[0][0].set_xticklabels([])
+            self.axes_twins[0][0].set_ylabel('x   ', rotation=0)
+            self.axes_twins[0][1].set_xticks([])
+            self.axes_twins[0][1].set_xticklabels([])
+            self.axes_twins[0][1].set_ylabel('   vx', rotation=0)
+            self.axes_twins[1][0].set_xticks([])
+            self.axes_twins[1][0].set_xticklabels([])
+            self.axes_twins[1][0].set_ylabel('y   ', rotation=0)
+            self.axes_twins[1][1].set_xticks([])
+            self.axes_twins[1][1].set_xticklabels([])
+            self.axes_twins[1][1].set_ylabel('   vy', rotation=0)
+            self.axes_twins[2][0].set_xticks([])
+            self.axes_twins[2][0].set_xticklabels([])
+            self.axes_twins[2][0].set_ylabel('w   ', rotation=0)
+            self.axes_twins[2][1].set_xticks([])
+            self.axes_twins[2][1].set_xticklabels([])
+            self.axes_twins[2][1].set_ylabel('   vw', rotation=0)
+            self.fig.subplots_adjust(left=0.15, right=0.85)
 
             # draw:
             self.fig.canvas.draw_idle()
@@ -176,12 +294,36 @@ class Viewer:
             self.axes_twins[2][1].plot(spacing, data_dict['vyaw'], 'b')
 
             # update plot limits:
-            self.axes_twins[0][0].set_ylim(mins_dict['x'],      maxs_dict['x'])
-            self.axes_twins[0][1].set_ylim(mins_dict['vx'],     maxs_dict['vx'])
-            self.axes_twins[1][0].set_ylim(mins_dict['y'],      maxs_dict['y'])
-            #self.axes_twins[1][1].set_ylim(mins_dict['vy'],     maxs_dict['vy']) # all zeros
-            self.axes_twins[2][0].set_ylim(mins_dict['yaw'],    maxs_dict['yaw'])
-            self.axes_twins[2][1].set_ylim(mins_dict['vyaw'],   maxs_dict['vyaw'])
+            E = 0.2
+            with warnings.catch_warnings():
+                warnings.simplefilter("ignore")
+                self.axes_twins[0][0].set_ylim(mins_dict['x']    - (maxs_dict['x']    - mins_dict['x'])*E,    maxs_dict['x']    + (maxs_dict['x']    - mins_dict['x'])*E)
+                self.axes_twins[0][1].set_ylim(mins_dict['vx']   - (maxs_dict['vx']   - mins_dict['vx'])*E,   maxs_dict['vx']   + (maxs_dict['vx']   - mins_dict['vx'])*E)
+                self.axes_twins[1][0].set_ylim(mins_dict['y']    - (maxs_dict['y']    - mins_dict['y'])*E,    maxs_dict['y']    + (maxs_dict['y']    - mins_dict['y'])*E)
+                self.axes_twins[1][1].set_ylim(mins_dict['vy']   - (maxs_dict['vy']   - mins_dict['vy'])*E,   maxs_dict['vy']   + (maxs_dict['vy']   - mins_dict['vy'])*E)
+                self.axes_twins[2][0].set_ylim(mins_dict['yaw']  - (maxs_dict['yaw']  - mins_dict['yaw'])*E,  maxs_dict['yaw']  + (maxs_dict['yaw']  - mins_dict['yaw'])*E)
+                self.axes_twins[2][1].set_ylim(mins_dict['vyaw'] - (maxs_dict['vyaw'] - mins_dict['vyaw'])*E, maxs_dict['vyaw'] + (maxs_dict['vyaw'] - mins_dict['vyaw'])*E)
+            
+            # disable ticks and labels on x axis
+            self.axes_twins[0][0].set_xticks([])
+            self.axes_twins[0][0].set_xticklabels([])
+            self.axes_twins[0][0].set_ylabel('x   ', rotation=0)
+            self.axes_twins[0][1].set_xticks([])
+            self.axes_twins[0][1].set_xticklabels([])
+            self.axes_twins[0][1].set_ylabel('   vx', rotation=0)
+            self.axes_twins[1][0].set_xticks([])
+            self.axes_twins[1][0].set_xticklabels([])
+            self.axes_twins[1][0].set_ylabel('y   ', rotation=0)
+            self.axes_twins[1][1].set_xticks([])
+            self.axes_twins[1][1].set_xticklabels([])
+            self.axes_twins[1][1].set_ylabel('   vy', rotation=0)
+            self.axes_twins[2][0].set_xticks([])
+            self.axes_twins[2][0].set_xticklabels([])
+            self.axes_twins[2][0].set_ylabel('w   ', rotation=0)
+            self.axes_twins[2][1].set_xticks([])
+            self.axes_twins[2][1].set_xticklabels([])
+            self.axes_twins[2][1].set_ylabel('   vw', rotation=0)
+            self.fig.subplots_adjust(left=0.15, right=0.85)
 
             # draw:
             self.fig.canvas.draw_idle()
@@ -192,7 +334,7 @@ if __name__ == '__main__':
         parser = ap.ArgumentParser(prog="vector viewer", 
                                 description="ROS Topic Throttle Tool",
                                 epilog="Maintainer: Owen Claxton (claxtono@qut.edu.au)")
-        parser.add_argument('--mode', '-m', type=check_string, choices=["imu","odom"], default="odom", help="Specify ROS log level (default: %(default)s).")
+        parser.add_argument('--mode', '-m', type=check_string, choices=["imu","odom", "dOdom"], default="odom", help="Specify ROS log level (default: %(default)s).")
         parser.add_argument('--topic', '-t', type=check_string, default=None, help='Set node rate (default: %(default)s).')
         parser.add_argument('--rate', '-r', type=check_positive_float, default=10.0, help='Set node rate (default: %(default)s).')
         parser.add_argument('--node-name', '-N', default="view_vector", help="Specify node name (default: %(default)s).")
@@ -213,4 +355,4 @@ if __name__ == '__main__':
         viewer.main()
         rospy.loginfo("Exit state reached.")
     except rospy.ROSInterruptException:
-        pass
+        plt.close('all')
