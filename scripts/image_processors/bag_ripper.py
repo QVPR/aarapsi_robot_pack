712088922f784d270598b2443ade8f47c68c2559
Owen Claxton <claxtono@qut.edu.au>
2023-04-27 07:32:38 +1000

    Extraction of rosbag reading methods

--- scripts/image_processors/bag_ripper.py
+++ scripts/image_processors/bag_ripper.py
@@ -15,7 +15,7 @@ from cv_bridge import CvBridge
 from pathlib import Path
 
 from pyaarapsi.core.argparse_tools import check_positive_float, check_bool, check_string, check_string_list
-from pyaarapsi.core.ros_tools import roslogger, LogType, yaw_from_q
+from pyaarapsi.core.ros_tools import roslogger, LogType, process_bag
 from pyaarapsi.core.helper_tools import vis_dict, formatException
 
 '''
@@ -24,120 +24,77 @@ ROS Bag Ripper
 
 Open up a bag, grab out odom_topic and img_topics, and save to a compressed npz library
 
-'''
+'''        
 
 class mrc:
-    def __init__(self, odom_topic, img_topics, bag_path, rate, compress, save_path, save_name):
+    def __init__(self, odom_topic, img_topics, bag_path, bag_name, rate, compress, save_path, save_name):
         self.odom_topic     = odom_topic
         if compress:
             self.img_topics = [i + "/compressed" if not i.endswith("/compressed") else i for i in img_topics]
         else:
             self.img_topics = img_topics
         self.img_types      = [CompressedImage if i.endswith("/compressed") else Image for i in self.img_topics]
-        self.topic_list     = [self.odom_topic] + self.img_topics
 
         self.bag_path       = bag_path
-        self.bag_dir        = os.path.dirname(self.bag_path)
-        self.bag_name       = os.path.basename(self.bag_path)
+        self.bag_name       = bag_name
         self.rate           = rate
         self.compress       = compress
         self.save_path      = save_path
         self.save_name      = save_name
 
-        Path(self.bag_dir).mkdir(parents=False, exist_ok=True)
+        Path(self.bag_path).mkdir(parents=False, exist_ok=True)
         Path(self.save_path).mkdir(parents=False, exist_ok=True)
 
-    def rip_bag(self):
-        # Read rosbag
-        roslogger("Ripping through rosbag ...", LogType.INFO)
-        with rosbag.Bag(self.bag_path, 'r') as ros_bag:
-            for topic, msg, timestamp in tqdm(ros_bag.read_messages(topics=self.topic_list)):
-                
-                if topic in self.img_topics:
-                    self.img_msgs[topic] = msg
-                elif topic == self.odom_topic:
-                    self.odom_msg = msg
-                    if self.logged_t == -1:
-                        self.logged_t = timestamp.to_sec()
-                    elif timestamp.to_sec() - self.logged_t > 1/self.rate:
-                        self.odom_arr.append(self.odom_msg)
-                        self.imgs_arr.append(self.img_msgs)
-                        self.logged_t = timestamp.to_sec()
-                else:
-                    raise Exception("Type not in img_topics or odom_topic; how did we hit this? topic: %s" % (str(topic)))
-
-    def convert_bag(self):
-        roslogger("Converting stored messages (%s)" % (str(len(self.imgs_arr))), LogType.INFO)
-        bridge = CvBridge()
-        self.odom_dict = {'position': {'x': [], 'y': [], 'yaw': []}, 'velocity': {'x': [], 'y': [], 'yaw': []}}
-        self.imgs_dict = {key: [] for key in list(self.img_msgs.keys())}
-
-        compress_func = lambda msg: bridge.compressed_imgmsg_to_cv2(msg, "bgr8")
-        raw_img_func = lambda msg: bridge.imgmsg_to_cv2(msg, "passthrough")
-
-        for (new_odom, new_imgs) in tqdm(zip(self.odom_arr, self.imgs_arr)):
-            self.odom_dict['position']['x'].append(new_odom.pose.pose.position.x)
-            self.odom_dict['position']['y'].append(new_odom.pose.pose.position.y)
-            self.odom_dict['position']['yaw'].append(yaw_from_q(new_odom.pose.pose.orientation))
-
-            self.odom_dict['velocity']['x'].append(new_odom.twist.twist.linear.x)
-            self.odom_dict['velocity']['y'].append(new_odom.twist.twist.linear.y)
-            self.odom_dict['velocity']['yaw'].append(new_odom.twist.twist.angular.z)
-
-            for topic in list(new_imgs.keys()):
-                if "/compressed" in topic:
-                    self.imgs_dict[topic].append(compress_func(new_imgs[topic]))
-                else:
-                    self.imgs_dict[topic].append(raw_img_func(new_imgs[topic]))
-
-        self.master_dict = {'odom': self.odom_dict, 'imgs': self.imgs_dict}
-
-    def main(self):    
-        self.odom_msg = Odometry()
-        self.img_msgs = {topic: type() for type, topic in zip(self.img_types, self.img_topics)}
-        self.odom_arr = []
-        self.imgs_arr = []  
-        self.logged_t = -1
-
-        self.rip_bag()
-        self.convert_bag()
-        vis_dict(self.master_dict)
-        np.savez(self.save_path + "/" + self.save_name, **self.master_dict)
+    def main(self):
+        new_dict = process_bag(self.bag_path + '/' + self.bag_name, self.rate, self.odom_topic, self.img_topics, printer=lambda x: roslogger(x, LogType.INFO))
+        vis_dict(new_dict)
+        np.savez(self.save_path + "/" + self.save_name, **new_dict)
         
 if __name__ == '__main__':
     parser = ap.ArgumentParser(prog="Bag Ripper", 
                             description="ROS Bag Ripper Tool",
                             epilog="Maintainer: Owen Claxton (claxtono@qut.edu.au)")
     
-    save_path = rospkg.RosPack().get_path(rospkg.get_package_name(os.path.abspath(__file__))) + "/data/ripped_bags"
-    # Positional Arguments:
-    parser.add_argument('odom-topic',           type=check_string,                              help='Set odometry topic.')
-    parser.add_argument('img-topics',           type=check_string_list,                         help='Set image topics (/compressed can be added to each to mix compressed and uncompressed topics).')
-    parser.add_argument('bag-path',             type=check_string,                              help='Set system file path (including file name) for bag to read.')
+    sp_default = rospkg.RosPack().get_path(rospkg.get_package_name(os.path.abspath(__file__))) + "/data/ripped_bags"
+    try:
+        bp_default = os.environ['AARAPSI_ONEDRIVE_ROOT'] + '/3.0 Data/3.1 rosbags/improved'
+    except:
+        roslogger("Environment variable AARAPSI_ONEDRIVE_ROOT not set, defaulting to HOME as root instead.", LogType.WARN, ros=False)
+        roslogger("AARAPSI_ONEDRIVE_ROOT should specify directory to (and including) aarapsi_home, i.e. /path/example/aarapsi_home.", LogType.WARN, ros=False)
+        bp_default = os.environ['HOME'] + '/OneDrive/aarapsi_home/3.0 Data/3.1 rosbags/improved'
+    ot_default = '/odom/filtered'
+    it_default = '/ros_indigosdk_occam/image0'
 
-    # Optional Arguments:
-    parser.add_argument('--rate', '-r',         type=check_positive_float, default=10.0,        help='Set sample rate (default: %(default)s).')
-    parser.add_argument('--compress', '-C',     type=check_bool,           default=False,       help='Force use of only compressed image topics (default: %(default)s).')
-    parser.add_argument('--save-path', '-Sp',   type=check_string,         default=save_path,   help='Set output .npz file path (excluding file name) (default: %(default)s).')
-    parser.add_argument('--save-name', '-Sn',   type=check_string,         default=None,        help='Set output .npz file name (default: %(default)s).')
+    parser.add_argument('bag-name',             type=check_string,                             help='Set input .bag file name.')
+    parser.add_argument('--odom-topic', '-Ot',  type=check_string,         default=ot_default, help='Set odometry topic.')
+    parser.add_argument('--img-topics', '-It',  type=check_string_list,    default=it_default, help='Set image topics (/compressed can be added to each to mix compressed and uncompressed topics).')
+    parser.add_argument('--bag-path',   '-Bp',  type=check_string,         default=bp_default, help='Set system directory (excluding file name) for bag to read.')
+    parser.add_argument('--save-path',  '-Sp',  type=check_string,         default=sp_default, help='Set output .npz file path (excluding file name) (default: %(default)s).')
+    parser.add_argument('--save-name',  '-Sn',  type=check_string,         default='rip.npz',  help='Set output .npz file name (default: %(default)s).')
+    parser.add_argument('--rate',       '-r',   type=check_positive_float, default=10.0,       help='Set sample rate (default: %(default)s).')
+    parser.add_argument('--compress',   '-C',   type=check_bool,           default=True,       help='Force use of only compressed image topics (default: %(default)s).')
     
     raw_args    = parser.parse_known_args()
     args        = vars(raw_args[0])
 
-    odom_topic  = args['odom-topic']
-    img_topics  = args['img-topics']
-    bag_path    = args['bag-path']
-
-    rate        = args['rate']
-    compress    = args['compress']
+    bag_name    = args['bag-name']
+    odom_topic  = args['odom_topic']
+    img_topics  = args['img_topics']
+    bag_path    = args['bag_path']
     save_path   = args['save_path']
     save_name   = args['save_name']
+    rate        = args['rate']
+    compress    = args['compress']
 
-    nmrc = mrc(odom_topic, img_topics, bag_path, rate, compress, save_path, save_name)
     try:
+        nmrc = mrc(odom_topic, img_topics, bag_path, bag_name, rate, compress, save_path, save_name)
         nmrc.main()
-        roslogger("Operation complete.", LogType.INFO)
+        roslogger("Operation complete.", LogType.INFO, ros=False) # False as rosnode likely terminated
         sys.exit()
+    except SystemExit as e:
+        pass
+    except ConnectionRefusedError as e:
+        roslogger("Error: Is the roscore running and accessible?", LogType.ERROR, ros=False) # False as rosnode likely terminated
     except:
-        roslogger(formatException(), LogType.ERROR)
-        roslogger("Error state reached, system exit triggered.", LogType.INFO)
+        roslogger("Error state reached, system exit triggered.", LogType.WARN, ros=False) # False as rosnode likely terminated
+        roslogger(formatException(), LogType.ERROR, ros=False)
