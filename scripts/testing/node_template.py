e75d1344450f8f40d42e178ffb4033494c06505b
Owen Claxton <claxtono@qut.edu.au>
2023-05-15 13:52:39 +1000

    updated node_template

--- scripts/testing/node_template.py
+++ scripts/testing/node_template.py
@@ -5,76 +5,94 @@ import argparse as ap
 import sys
 from std_msgs.msg import String
 from pyaarapsi.core.argparse_tools import check_positive_float, check_positive_int, check_bool, check_string
-from pyaarapsi.core.ros_tools import Heartbeat, NodeState, roslogger, LogType, ROS_Param_Server, set_rospy_log_lvl
+from pyaarapsi.core.ros_tools import NodeState, roslogger, LogType, set_rospy_log_lvl, init_node
 from pyaarapsi.core.helper_tools import formatException
 
 '''
-Node Name Goes Here
+Node Name
 
-Node description goes here.
+Node description.
 
 '''
 
 class mrc():
-    def __init__(self, node_name, rate, namespace, anon, log_level, reset):
+    def __init__(self, node_name, rate_num, namespace, anon, log_level, reset, order_id=0):
 
-        self.node_name      = node_name
-        self.namespace      = namespace
-        self.nodespace      = self.namespace + "/" + self.node_name
-        self.anon           = anon
+        init_node(self, node_name, namespace, rate_num, anon, log_level, order_id=order_id, throttle=30)
 
-        self.params         = ROS_Param_Server()
-        self.log_level      = self.params.add(self.nodespace + "/log_level", log_level, check_positive_int,     force=reset)
-        self.rate_num       = self.params.add(self.nodespace + "/rate",      rate,      check_positive_float,   force=reset)
-    
-        rospy.init_node(self.node_name, anonymous=self.anon, log_level=self.log_level.get())
-        roslogger('Starting [%s] %s node.' % (self.namespace, self.node_name), LogType.INFO, ros=True)
-        self.rate_obj       = rospy.Rate(self.rate_num.get())
-        self.heartbeat      = Heartbeat(self.node_name, self.namespace, NodeState.INIT, self.rate_num)
-        self.param_sub      = rospy.Subscriber(self.namespace + "/params_update", String, self.param_cb, queue_size=100)
+        self.init_params(rate_num, log_level, reset)
+        self.init_vars()
+        self.init_rospy()
 
-    def param_cb(self, msg):
-        if self.params.exists(msg.data):
-            roslogger("Change to parameter [%s]; logged." % msg.data, LogType.DEBUG, ros=True)
-            self.params.update(msg.data)
+        self.main_ready      = True
+        rospy.set_param(self.namespace + '/launch_step', order_id + 1)
 
-            if msg.data == self.log_level.name:
-                set_rospy_log_lvl(self.log_level.get())
-            elif msg.data == self.rate_num.name:
-                self.rate_obj = rospy.Rate(self.rate_num.get())
+    def init_params(self, rate_num, log_level, reset):
+        self.LOG_LEVEL       = self.ROS_HOME.params.add(self.nodespace + "/log_level",           log_level,              check_positive_int,                             force=reset)
+        self.RATE_NUM        = self.ROS_HOME.params.add(self.nodespace + "/rate",                rate_num,               check_positive_float,                           force=reset)
+
+    def init_vars(self):
+        pass
+
+    def init_rospy(self):
+        self.rate_obj        = rospy.Rate(self.RATE_NUM.get())
+        self.param_sub       = rospy.Subscriber(self.namespace + "/params_update", String, self.param_callback, queue_size=100)
+
+    def param_callback(self, msg):
+        self.parameters_ready = False
+        if self.ROS_HOME.params.exists(msg.data):
+            if not self.ROS_HOME.params.update(msg.data):
+                self.print("Change to parameter [%s]; bad value." % msg.data, LogType.DEBUG)
+        
+            else:
+                self.print("Change to parameter [%s]; updated." % msg.data, LogType.DEBUG)
+
+                if msg.data == self.LOG_LEVEL.name:
+                    set_rospy_log_lvl(self.LOG_LEVEL.get())
+                elif msg.data == self.RATE_NUM.name:
+                    self.rate_obj = rospy.Rate(self.RATE_NUM.get())
         else:
-            roslogger("Change to untracked parameter [%s]; ignored." % msg.data, LogType.DEBUG, ros=True)
+            self.print("Change to untracked parameter [%s]; ignored." % msg.data, LogType.DEBUG)
+        self.parameters_ready = True
 
     def main(self):
-        self.heartbeat.set_state(NodeState.MAIN)
+        self.ROS_HOME.set_state(NodeState.MAIN)
 
         while not rospy.is_shutdown():
             self.rate_obj.sleep()
 
+    def print(self, text, logtype=LogType.INFO, throttle=0, ros=None, name=None, no_stamp=None):
+        if ros is None:
+            ros = self.ROS_HOME.logros
+        if name is None:
+            name = self.ROS_HOME.node_name
+        if no_stamp is None:
+            no_stamp = self.ROS_HOME.logstamp
+        roslogger(text, logtype, throttle=throttle, ros=ros, name=name, no_stamp=no_stamp)
+
+    def exit(self):
+        self.print("Quit received.")
+        sys.exit()
+
+def do_args():
+    parser = ap.ArgumentParser(prog="file_name.py", 
+                            description="Node Name",
+                            epilog="Maintainer: Your Name (your.email@email.com)")
+    parser.add_argument('--node-name',        '-N',  type=check_string,                 default="file_name",      help="Specify node name (default: %(default)s).")
+    parser.add_argument('--rate',             '-r',  type=check_positive_float,         default=2.0,              help='Specify node rate (default: %(default)s).')
+    parser.add_argument('--anon',             '-a',  type=check_bool,                   default=False,            help="Specify whether node should be anonymous (default: %(default)s).")
+    parser.add_argument('--namespace',        '-n',  type=check_string,                 default="/vpr_nodes",     help="Specify ROS namespace (default: %(default)s).")
+    parser.add_argument('--log-level',        '-V',  type=int, choices=[1,2,4,8,16],    default=2,                help="Specify ROS log level (default: %(default)s).")
+    parser.add_argument('--reset',            '-R',  type=check_bool,                   default=False,            help='Force reset of parameters to specified ones (default: %(default)s)')
+    parser.add_argument('--order-id',         '-ID', type=int,                          default=0,                help='Specify boot order of pipeline nodes (default: %(default)s).')
+
+    raw_args = parser.parse_known_args()
+    return vars(raw_args[0])
 
 if __name__ == '__main__':
-    parser = ap.ArgumentParser(prog="file_name_goes_here.py", 
-                            description="Node Name Goes Here",
-                            epilog="Maintainer: Your Name (your.email@email.com)")
-    parser.add_argument('--node-name', '-N', type=check_string,              default="file_name_goes_here", help="Specify node name (default: %(default)s).")
-    parser.add_argument('--rate',      '-r', type=check_positive_float,      default=2.0,                   help='Specify node rate (default: %(default)s).')
-    parser.add_argument('--anon',      '-a', type=check_bool,                default=False,                 help="Specify whether node should be anonymous (default: %(default)s).")
-    parser.add_argument('--namespace', '-n', type=check_string,              default="/vpr_nodes",          help="Specify ROS namespace (default: %(default)s).")
-    parser.add_argument('--log-level', '-V', type=int, choices=[1,2,4,8,16], default=2,                     help="Specify ROS log level (default: %(default)s).")
-    parser.add_argument('--reset',     '-R', type=check_bool,                default=False,                 help='Force reset of parameters to specified ones (default: %(default)s)')
-    
-    raw_args = parser.parse_known_args()
-    args = vars(raw_args[0])
-
-    node_name   = args['node_name']
-    rate        = args['rate']
-    namespace   = args['namespace']
-    anon        = args['anon']
-    log_level   = args['log_level']
-    reset       = args['reset']
-
     try:
-        nmrc = mrc(node_name, rate, namespace, anon, log_level, reset)
+        args = do_args()
+        nmrc = mrc(args['node_name'], args['rate'], args['namespace'], args['anon'], args['log_level'], args['reset'], order_id=args['order_id'])
         nmrc.main()
         roslogger("Operation complete.", LogType.INFO, ros=False) # False as rosnode likely terminated
         sys.exit()
